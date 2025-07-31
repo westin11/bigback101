aa5bb96e9ac38f830647c8f80abc85f57de51287
bigback101 <westin0728@icloud.com>
2025-07-31 10:59:44 -0700

    button fix

--- scripd.js
+++ scripd.js
@@ -2,4 +2,4 @@ const btn = document.querySelector("button")
 function checkbutton(){
     window.open("J.Geco - Chicken Song.mp4")
 }
-addEventListener("click", checkbutton)
+btn.addEventListener("click", checkbutton)
